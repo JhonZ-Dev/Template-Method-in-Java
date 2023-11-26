f3723ec1f649d06c4f57e551ebaaa2de02392cc3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-26 17:07:33 -0500

    Update Main.java

--- Template Method/src/Main.java
+++ Template Method/src/Main.java
@@ -1,5 +1,11 @@
+import procesamiento_texto.DocumentProcessor;
+import procesamiento_texto.TextDocumentProcessor;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        DocumentProcessor textoDocumento = new TextDocumentProcessor();
+        textoDocumento.processDocument();
     }
 }
