7b088c85ab5168ae7cf2e6f4ee6346288b06bda3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-26 17:01:30 -0500

    fdfd

--- Template Method/src/TextDocumentProcessor.java
+++ Template Method/src/TextDocumentProcessor.java
@@ -1,6 +1,8 @@
 public class TextDocumentProcessor extends DocumentProcessor{
     @Override
     protected void processContent() {
+        // Lógica específica para procesar contenido de un documento de texto
+        System.out.println("Procesando contenido de documento de texto...");
 
     }
 }
