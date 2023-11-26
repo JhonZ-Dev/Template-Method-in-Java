296949b47c6ae5ef8045d94e080433321f1f35cd
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-26 17:05:16 -0500

    Update procesadorDocumentoHojaCalculo.java

--- Template Method/src/procesamiento_texto/procesadorDocumentoHojaCalculo.java
+++ Template Method/src/procesamiento_texto/procesadorDocumentoHojaCalculo.java
@@ -4,6 +4,7 @@ public class procesadorDocumentoHojaCalculo extends DocumentProcessor{
 
     @Override
     protected void processContent() {
-        
+        // Lógica específica para procesar contenido de una hoja de cálculo
+        System.out.println("Procesando contenido de hoja de cálculo...");
     }
 }
