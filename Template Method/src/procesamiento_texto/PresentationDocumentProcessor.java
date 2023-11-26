edc9413e0e45d3b5952f0cbae690973c64aee287
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-26 17:06:33 -0500

    Update PresentationDocumentProcessor.java

--- Template Method/src/procesamiento_texto/PresentationDocumentProcessor.java
+++ Template Method/src/procesamiento_texto/PresentationDocumentProcessor.java
@@ -3,6 +3,7 @@ package procesamiento_texto;
 public class PresentationDocumentProcessor extends DocumentProcessor{
     @Override
     protected void processContent() {
-        
+        // Lógica específica para procesar contenido de una presentación
+        System.out.println("Procesando contenido de presentación...");
     }
 }
