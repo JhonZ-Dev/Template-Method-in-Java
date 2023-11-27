f0658344c6012db685ce7f3e194f2ff997c3c65c
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-26 21:11:22 -0500

    Update Main.java

--- Template Method/src/Main.java
+++ Template Method/src/Main.java
@@ -5,15 +5,20 @@ import procesamiento_texto.procesadorDocumentoHojaCalculo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("");
+        System.out.println("");
         System.out.println("***********Documento de texto***********************\n");
         DocumentProcessor textoDocumento = new TextDocumentProcessor();
         textoDocumento.processDocument();
 
+        System.out.println("");
+        System.out.println("");
         System.out.println("***********Documento de hoja de calculo***********************\n");
         DocumentProcessor hojadecalculo = new procesadorDocumentoHojaCalculo();
         hojadecalculo.processDocument();
 
+        System.out.println("");
+        System.out.println("");
         System.out.println("***********Documento de presentaciones***********************\n");
         DocumentProcessor presentaciones = new PresentationDocumentProcessor();
         presentaciones.processDocument();
